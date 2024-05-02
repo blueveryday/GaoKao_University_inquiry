846787fe1c5ccc8fafb45d9d9698d1fe25e24944
blueveryday <blueveryday@163.com>
2024-05-03 00:51:42 +0800

    修复专业分数线，招生计划因页数缺失引起的数据不完整问题。

--- runme.py
+++ runme.py
@@ -7,7 +7,8 @@ import platform
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def download_file(url, local_filename, headers=None):
+def download_file(url, local_filename):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
     # 发起GET请求下载文件
     with requests.get(url, stream=True, headers=headers) as response:
         response.raise_for_status()
@@ -18,7 +19,7 @@ def download_file(url, local_filename, headers=None):
                 if chunk:
                     f.write(chunk)
 
-def run_code(choice, headers=None):    
+def run_code(choice):    
     if choice == 1:
         clear_screen()
         # 各省分数线
@@ -49,9 +50,7 @@ def run_code(choice, headers=None):
         if not os.path.exists(source_folder):
             os.makedirs(source_folder)
 
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
-
-        download_file(url, local_filename, headers=headers)
+        download_file(url, local_filename)
 
         with open('source/gsfsx.json', encoding='utf-8') as f:
             data = json.loads(f.read())
@@ -84,27 +83,14 @@ def run_code(choice, headers=None):
     elif choice == 2:
         clear_screen()
         # 专业分数线
+        # 人机交互式输入 school_id 和 year 数据
         local_province_id = input("请输入新高考的省市区代码（渝:50，其他可以查看Province_ID.txt）: ")
         local_type_id = input("请输入文理科代码（2073代表物理类，2074代表历史类）")
         school_id = input("请输入学校ID（比如清华大学:140）: ")
+        total_pages = int(input("(输入前请从网页查询)请输入专业分数线的总页数: "))  # 输入总页数
         year = input("请输入录取年份: ")
 
-        base_url = 'https://api.zjzw.cn/web/api/?'
-        parameters = {
-            'local_batch_id': '14',
-            'local_province_id': local_province_id,
-            'local_type_id': local_type_id,
-            'page': '1',
-            'school_id': school_id,
-            'size': '10',
-            'special_group': '',
-            'uri': 'apidata/api/gk/score/special',
-            'year': year
-        }
-        url = base_url + '&'.join([f"{key}={value}" for key, value in parameters.items()])
-        local_filename = 'source/zyfsx.json'
-
-        # 检查并创建 csv 和 source 文件夹
+        # 创建保存 CSV 文件的文件夹
         csv_folder = 'csv'
         if not os.path.exists(csv_folder):
             os.makedirs(csv_folder)
@@ -112,31 +98,70 @@ def run_code(choice, headers=None):
         if not os.path.exists(source_folder):
             os.makedirs(source_folder)
 
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
-        download_file(url, local_filename, headers=headers)
+        # 创建一个列表来存储所有页面的数据
+        all_items = []
 
-        with open('source/zyfsx.json', encoding='utf-8') as f:
-            data = json.loads(f.read())
-            items = data['data']['item']
+        # 循环处理每一页
+        for page_id in range(1, total_pages + 1):
+            # 定义要下载的文件URL和本地保存路径
+            base_url = 'https://api.zjzw.cn/web/api/?'
+            parameters = {
+                'local_batch_id': '14',                      # 录取批次
+                'local_province_id': local_province_id,      # 省市区代码
+                'local_type_id': local_type_id,              # 文理科
+                'page': str(page_id),                        # 网页页码总数
+                'school_id': school_id,                      # 学校id
+                'size': '10',                                # 每页显示条目数
+                'special_group': '',                         #
+                'uri': 'apidata/api/gk/score/special',       # 路径
+                'year': year                                 # 录取年份
+            }
+            url = base_url + '&'.join([f"{key}={value}" for key, value in parameters.items()])
+            local_folder = 'source'
+            local_filename = os.path.join(local_folder, f'zyfsx_{page_id}.json')
 
-        csv_file_path = f"{csv_folder}/学校ID-{school_id}_{items[0]['name']}_{items[0]['local_province_name']}_{year}_专业分数线.csv"
+            # 创建保存 JSON 文件的文件夹
+            if not os.path.exists(local_folder):
+                os.makedirs(local_folder)
+            if not os.path.exists(source_folder):
+                os.makedirs(source_folder)
 
+            # 下载文件
+            download_file(url, local_filename)
+
+            # 读取 JSON 文件
+            with open(local_filename, encoding='utf-8') as f:
+                data = json.loads(f.read())
+                items = data['data']['item']
+                all_items.extend(items)
+
+        # 获取第一个项目的名称
+        first_item_name = all_items[0]['name']
+        two_item_name = all_items[0]['local_province_name']
+
+        # 定义要保存的 CSV 文件路径
+        csv_file_path = os.path.join(csv_folder, f"学校ID-{school_id}_{first_item_name}_{two_item_name}_{year}_专业分数线.csv")   
+
+        # 打开 CSV 文件并写入数据
         with open(csv_file_path, mode='w', newline='', encoding='utf-8-sig') as csv_file:
             writer = csv.writer(csv_file)
-            writer.writerow(["学校名称", "招生年份", "录取批次", "省市区", "文理科", "最低分", "最低位次", "平均分", "选科要求", "专业名称"])
-            for item in items:
-                name = item['name']
-                year = item['year']
-                local_batch_name = item['local_batch_name']
-                local_province_name = item['local_province_name']
-                local_type_name = item['local_type_name']
-                min_score = item['min']
-                min_section = item['min_section']
-                average = item['average']
-                sp_info = item['sp_info']
-                spname = item['spname']
+    
+            # 写入表头
+            writer.writerow(["学校名称", "录取批次", "省市区", "文理科", "最低分", "最低位次", "平均分", "选科要求", "专业名称"])
 
-                writer.writerow([name, year, local_batch_name, local_province_name, local_type_name, min_score, min_section, average, sp_info, spname])
+            # 写入数据到 CSV 文件
+            for item in all_items:
+                name = item['name']                                  #学校名称
+                local_batch_name = item['local_batch_name']          #录取批次
+                local_province_name = item['local_province_name']    #省市区
+                local_type_name = item['local_type_name']            #文理科
+                min_score = item['min']                              #最低分
+                min_section = item['min_section']                    #最低位次
+                average = item['average']                            #平均分
+                sp_info = item['sp_info']                            #选科要求
+                spname = item['spname']                              #专业名称
+
+                writer.writerow([name, local_batch_name, local_province_name, local_type_name, min_score, min_section, average, sp_info, spname])
 
         print(f"数据已成功保存到 {csv_file_path} 文件中。")
         input("按 Enter 键继续...")
@@ -146,24 +171,10 @@ def run_code(choice, headers=None):
         local_province_id = input("请输入新高考的省市区代码（渝:50，其他可以查看Province_ID.txt）: ")
         local_type_id = input("请输入文理科代码（2073代表物理类，2074代表历史类）")
         school_id = input("请输入学校ID（比如清华大学:140）: ")
+        total_pages = int(input("(输入前请从网页查询)请输入招生计划的总页数: "))  # 输入总页数
         year = input("请输入录取年份: ")
 
-        base_url = 'https://api.zjzw.cn/web/api/?'
-        parameters = {
-            'local_batch_id': '14',
-            'local_province_id': local_province_id,
-            'local_type_id': local_type_id,
-            'page': '1',
-            'school_id': school_id,
-            'size': '10',
-            'special_group': '',
-            'uri': 'apidata/api/gkv3/plan/school',
-            'year': year
-        }
-        url = base_url + '&'.join([f"{key}={value}" for key, value in parameters.items()])
-        local_filename = 'source/zsjh.json'
-
-        # 检查并创建 csv 和 source 文件夹
+        # 创建保存 CSV 文件的文件夹
         csv_folder = 'csv'
         if not os.path.exists(csv_folder):
             os.makedirs(csv_folder)
@@ -171,29 +182,69 @@ def run_code(choice, headers=None):
         if not os.path.exists(source_folder):
             os.makedirs(source_folder)
 
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
-        download_file(url, local_filename, headers=headers)
+        # 创建一个列表来存储所有页面的数据
+        all_items = []
 
-        with open('source/zsjh.json', encoding='utf-8') as f:
-            data = json.loads(f.read())
-            items = data['data']['item']
+        # 循环处理每一页
+        for page_id in range(1, total_pages + 1):
+            # 定义要下载的文件URL和本地保存路径
+            base_url = 'https://api.zjzw.cn/web/api/?'
+            parameters = {
+                'local_batch_id': '14',                      # 录取批次
+                'local_province_id': local_province_id,      # 省市区代码
+                'local_type_id': local_type_id,              # 文理科
+                'page': str(page_id),                        # 网页页码总数
+                'school_id': school_id,                      # 学校id
+                'size': '10',                                # 每页显示条目数
+                'special_group': '',                         #
+                'uri': 'apidata/api/gkv3/plan/school',       #路径
+                'year': year                                 #录取年份
+            }
+            url = base_url + '&'.join([f"{key}={value}" for key, value in parameters.items()])
+            local_folder = 'source'
+            local_filename = os.path.join(local_folder, f'zyfsx_{school_id}_{year}_{page_id}.json')
 
-        csv_file_path = f"{csv_folder}/学校ID-{school_id}_{items[0]['name']}_省市区代码-{local_province_id}_{year}_招生计划.csv"
+            # 创建保存 JSON 文件的文件夹
+            if not os.path.exists(local_folder):
+                os.makedirs(local_folder)
+            if not os.path.exists(source_folder):
+                os.makedirs(source_folder)
 
+            # 下载文件
+            download_file(url, local_filename)
+
+            # 读取 JSON 文件
+            with open(local_filename, encoding='utf-8') as f:
+                data = json.loads(f.read())
+                items = data['data']['item']
+                all_items.extend(items)
+
+        # 获取第一个项目的名称
+        first_item_name = all_items[0]['name']
+
+        # 定义要保存的 CSV 文件路径
+        csv_file_path = os.path.join(csv_folder, f"学校ID-{school_id}_{first_item_name}_省市区代码-{local_province_id}_{year}_招生计划.csv")
+
+        # 打开 CSV 文件并写入数据
         with open(csv_file_path, mode='w', newline='', encoding='utf-8-sig') as csv_file:
             writer = csv.writer(csv_file)
+
+            # 写入表头
             writer.writerow(["学校名称", "学校所在省份", "招生年份", "文理科", "录取批次", "专业名称", "计划招生", "学制", "学费", "选科要求"])
-            for item in items:
-                name = item['name']
-                province_name = item['province_name']
-                year = item['year']
-                local_type_name = item['local_type_name']
-                local_batch_name = item['local_batch_name']
-                spname = item['spname']
-                num = item['num']
-                length = item['length']
-                tuition = item['tuition']
-                sp_info = item['sp_info']
+
+            # 写入所有页面的数据到 CSV 文件
+            for item in all_items:
+                name = item['name']  # 学校名称
+                province_name = item['province_name']  # 学校所在省份
+                year = item['year']  # 招生年份
+                local_type_name = item['local_type_name']  # 文理科
+                local_batch_name = item['local_batch_name']  # 录取批次
+                spname = item['spname']  # 专业名称
+                num = item['num']  # 计划招生
+                length = item['length']  # 学制
+                tuition = item['tuition']  # 学费
+                sp_info = item['sp_info']  # 选科要求
+                # 写入CSV文件
                 writer.writerow([name, province_name, year, local_type_name, local_batch_name, spname, num, length, tuition, sp_info])
 
         print(f"数据已成功保存到 {csv_file_path} 文件中。")
@@ -286,8 +337,7 @@ def run_code(choice, headers=None):
         if not os.path.exists('source'):
             os.makedirs('source')
 
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
-        download_file(url, local_filename, headers=headers)
+        download_file(url, local_filename)
 
         with open('source/school_id.json', 'r', encoding='utf-8') as file:
             json_string = file.read()
--- 招生计划_保存为csv.py
+++ 招生计划_保存为csv.py
@@ -19,62 +19,79 @@ def download_file(url, local_filename):
 local_province_id = input("请输入新高考的省市区代码（渝:50，其他可以查看Province_ID.txt）: ")
 local_type_id = input("请输入文理科代码（2073代表物理类，2074代表历史类）")
 school_id = input("请输入学校ID（比如清华大学:140）: ")
+total_pages = int(input("(输入前请从网页查询)请输入招生计划的总页数: "))  # 输入总页数
 year = input("请输入录取年份: ")
-
-# 定义要下载的文件URL和本地保存路径
-base_url = 'https://api.zjzw.cn/web/api/?'
-parameters = {
-    'local_batch_id': '14',                      #录取批次
-    'local_province_id': local_province_id,      #省市区代码，50代表重庆
-    'local_type_id': local_type_id,              #文理科，2073代表理科，2074代表文科（大类招生）
-    'page': '1',                                 #网页页码数
-    'school_id': school_id,                      #学校id，由网站定义，可修改,参加ID.TXT文档
-    'size': '10',                                #每页显示条目数
-    'special_group': '',                         #
-    'uri': 'apidata/api/gkv3/plan/school',       #路径
-    'year': year                                 #录取年份
-}
-url = base_url + '&'.join([f"{key}={value}" for key, value in parameters.items()])
-local_folder = 'source'
-local_filename = os.path.join(local_folder, 'zsjh.json')
-
-# 创建保存 JSON 文件的文件夹
-if not os.path.exists(local_folder):
-    os.makedirs(local_folder)
-
-# 下载文件
-download_file(url, local_filename)
-
-# 读取 JSON 文件
-with open(local_filename, encoding='utf-8') as f:
-    data = json.loads(f.read())
-    items = data['data']['item']
 
 # 创建保存 CSV 文件的文件夹
 csv_folder = 'csv'
 if not os.path.exists(csv_folder):
     os.makedirs(csv_folder)
+source_folder = 'source'
+if not os.path.exists(source_folder):
+    os.makedirs(source_folder)
+
+# 创建一个列表来存储所有页面的数据
+all_items = []
+
+# 循环处理每一页
+for page_id in range(1, total_pages + 1):
+    # 定义要下载的文件URL和本地保存路径
+    base_url = 'https://api.zjzw.cn/web/api/?'
+    parameters = {
+        'local_batch_id': '14',                      # 录取批次
+        'local_province_id': local_province_id,      # 省市区代码
+        'local_type_id': local_type_id,              # 文理科
+        'page': str(page_id),                        # 网页页码总数
+        'school_id': school_id,                      # 学校id
+        'size': '10',                                # 每页显示条目数
+        'special_group': '',                         #
+        'uri': 'apidata/api/gkv3/plan/school',       #路径
+        'year': year                                 #录取年份
+    }
+    url = base_url + '&'.join([f"{key}={value}" for key, value in parameters.items()])
+    local_folder = 'source'
+    local_filename = os.path.join(local_folder, f'zyfsx_{school_id}_{year}_{page_id}.json')
+
+    # 创建保存 JSON 文件的文件夹
+    if not os.path.exists(local_folder):
+        os.makedirs(local_folder)
+    if not os.path.exists(source_folder):
+        os.makedirs(source_folder)
+
+    # 下载文件
+    download_file(url, local_filename)
+
+    # 读取 JSON 文件
+    with open(local_filename, encoding='utf-8') as f:
+        data = json.loads(f.read())
+        items = data['data']['item']
+        all_items.extend(items)
+
+# 获取第一个项目的名称
+first_item_name = all_items[0]['name']
 
 # 定义要保存的 CSV 文件路径
-csv_file_path = os.path.join(csv_folder, f"学校ID-{school_id}_{items[0]['name']}_省市区代码-{local_province_id}_{year}_招生计划.csv")
+csv_file_path = os.path.join(csv_folder, f"学校ID-{school_id}_{first_item_name}_省市区代码-{local_province_id}_{year}_招生计划.csv")
 
 # 打开 CSV 文件并写入数据
 with open(csv_file_path, mode='w', newline='', encoding='utf-8-sig') as csv_file:
     writer = csv.writer(csv_file)
+
     # 写入表头
     writer.writerow(["学校名称", "学校所在省份", "招生年份", "文理科", "录取批次", "专业名称", "计划招生", "学制", "学费", "选科要求"])
-    # 写入数据
-    for item in items:
-        name = item['name']                                 #学校名称
-        province_name = item['province_name']               #学校所在省份
-        year = item['year']                                 #招生年份
-        local_type_name = item['local_type_name']           #文理科
-        local_batch_name = item['local_batch_name']         #录取批次
-        spname = item['spname']                             #专业名称
-        num = item['num']                                   #计划招生
-        length = item['length']                             #学制
-        tuition = item['tuition']                           #学费
-        sp_info = item['sp_info']                           #选科要求
+
+    # 写入所有页面的数据到 CSV 文件
+    for item in all_items:
+        name = item['name']  # 学校名称
+        province_name = item['province_name']  # 学校所在省份
+        year = item['year']  # 招生年份
+        local_type_name = item['local_type_name']  # 文理科
+        local_batch_name = item['local_batch_name']  # 录取批次
+        spname = item['spname']  # 专业名称
+        num = item['num']  # 计划招生
+        length = item['length']  # 学制
+        tuition = item['tuition']  # 学费
+        sp_info = item['sp_info']  # 选科要求
         # 写入CSV文件
         writer.writerow([name, province_name, year, local_type_name, local_batch_name, spname, num, length, tuition, sp_info])
 

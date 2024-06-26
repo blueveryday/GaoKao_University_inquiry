e62e7935fe3f93cb1c36846516a3ee71ce8667c6
blueveryday <blueveryday@163.com>
2024-06-26 16:22:49 +0800

    优化一分一段查询流程

--- runme.py
+++ runme.py
@@ -204,98 +204,59 @@ def score_ranking_menu():
 
             choice = input("\n请输入选项：")
             if choice == '1':
+                global local_province_id, local_type_id, year
+                folder_path = os.path.join("src", str(local_province_id), "score_ranking")
+                os.makedirs(folder_path, exist_ok=True)
+                filepath = os.path.join(folder_path, f"lists_{local_province_id}_{year}_{local_type_id}.json")  # 设置文件路径
+                province_name_results = get_province_name(local_province_id)  # 查询省市区名称
+                if not os.path.exists(filepath):
+                    os.system("cls" if os.name == "nt" else "clear")
+                    print(Fore.RED + f"\n你查询的{year}年一分一段JSON文件不存在，请先自定义下载文件后再查询！\n" + Style.RESET_ALL)
+                    break  # 文件不存在，重新输入
+
                 while True:
-                    # 输入省市区代码
+                    # 提示输入查询高考分数
                     while True:
-                        local_province_id = input(Fore.GREEN + " ※ 请输入省市区代码" + Fore.RED + "（例如 50，默认值为50）: " + Style.RESET_ALL) or "50"
-                        if get_province_name(local_province_id) is None:
-                            print(Fore.RED + "无效的省市区代码，请重新输入。\n" + Style.RESET_ALL)
-                            continue
-                            os.system('cls' if os.name == 'nt' else 'clear')
+                        score_or_rank_input = input(Fore.GREEN + "\n ※ 请输入查询的高考分数：" + Style.RESET_ALL)
+                        if not score_or_rank_input.isdigit() or int(score_or_rank_input) < 0 or int(score_or_rank_input) > 750:
+                            print(Fore.RED + "错误：请输入0 - 750 之间的整数。" + Style.RESET_ALL)
                         else:
-                            break
+                            break  # 如果输入合法，则退出循环
 
-                    while True:
-                        year = input(Fore.GREEN + " ※ 请输入年份" + Fore.RED + "（例如 2016 - 2028之间的年份，默认值为2024）: " + Style.RESET_ALL) or "2024"
-                        if not year.isdigit() or int(year) not in range(2016, 2028):
-                            print(Fore.RED + "错误：请输入2016 - 2028之间的有效年份。" + Style.RESET_ALL)
-                            continue
-                        else:
-                            break  # 如果输入的年份有效，则退出循环
-
-                    if int(year) >= 2021:
-                        while True:
-                            local_type_id = input(Fore.GREEN + " ※ 请输入物理、历史类代码" + Fore.RED + "（2021年及之后，2073 代表物理类，2074 代表历史类，默认值为2073）: " + Style.RESET_ALL) or "2073"
-                            if local_type_id in ["2073", "2074"]:
-                                break  # 如果输入正确，跳出循环
-                            else:
-                                print(Fore.RED + "你输入的数字错误，请按照提示重新输入文理科代码！" + Style.RESET_ALL)
-                                print("2021年之后的文理科代码是：" + Fore.RED + "2073 代表物理类，2074 代表历史类。" + Style.RESET_ALL)
+                    search_results = search_json_data(filepath, score_or_rank_input)
+                    if search_results:
+                        print(Fore.GREEN + f"\n查询的结果如下（省市区：{province_name_results}，年份：{year}）：\n" + Style.RESET_ALL)
+                        for result in search_results:
+                            print(f"高考分数: {result['score']}")
+                            print(f"同分人数: {result['num']}")
+                            print(f"排名区间: {result['rank_range']}")
+                            print(f"累计人数: {result['total']}")
+                            print(f"批次: {result['batch_name']}\n")
+                            # 获取appositive_fraction数组中的参数
+                            for app_fraction in result["appositive_fraction"]:
+                                ls_year1 = app_fraction["year"]
+                                ls_score1 = app_fraction["score"]
+                                ls_rank_range1 = app_fraction["rank_range"]
+                                print(f"{ls_year1}年同位次考生得分:{ls_score1}, 排名区间：{ls_rank_range1}")
+                        print()  # 每个结果之间用空行分隔
                     else:
-                        while True:
-                            local_type_id = input(Fore.GREEN + " ※ 请输入文、理科代码" + Fore.RED + "（2021年之前（不含），1 代表理科，2 代表文科，默认值为1）: " + Style.RESET_ALL) or "1"
-                            if local_type_id in ["1", "2"]:
-                                break  # 如果输入正确，跳出循环
-                            else:
-                                print(Fore.RED + "你输入的数字错误，请按照提示重新输入文理科代码！" + Style.RESET_ALL)
-                                print("2021年之前的文理科代码是：" + Fore.RED + "1 代表理科，2 代表文科；" + Style.RESET_ALL)
+                        print(Fore.RED + "未找到与输入内容匹配的数据。" + Style.RESET_ALL)
 
-                    folder_path = os.path.join("src", str(local_province_id), "score_ranking")
-                    os.makedirs(folder_path, exist_ok=True)
-                    filepath = os.path.join(folder_path, f"lists_{local_province_id}_{year}_{local_type_id}.json")  # 设置文件路径
-                    province_name_results = get_province_name(local_province_id)  # 查询省市区名称
-
-                    if not os.path.exists(filepath):
-                        os.system("cls" if os.name == "nt" else "clear")
-                        print(Fore.RED + f"\n你查询的{year}年一分一段JSON文件不存在，请先自定义下载文件后再查询！\n" + Style.RESET_ALL)
-                        break  # 文件不存在，重新输入
-
+                    # 提示询问用户是否继续查询
                     while True:
-                        # 提示输入查询高考分数
-                        while True:
-                            score_or_rank_input = input(Fore.GREEN + " ※ 请输入查询的高考分数：" + Style.RESET_ALL)
-                            if not score_or_rank_input.isdigit() or int(score_or_rank_input) < 0 or int(score_or_rank_input) > 750:
-                                print(Fore.RED + "错误：请输入0 - 750 之间的整数。" + Style.RESET_ALL)
-                            else:
-                                break  # 如果输入合法，则退出循环
-
-                        search_results = search_json_data(filepath, score_or_rank_input)
-                        if search_results:
-                            print(Fore.GREEN + f"\n查询的结果如下（省市区：{province_name_results}，年份：{year}）：\n" + Style.RESET_ALL)
-                            for result in search_results:
-                                print(f"高考分数: {result['score']}")
-                                print(f"同分人数: {result['num']}")
-                                print(f"排名区间: {result['rank_range']}")
-                                print(f"累计人数: {result['total']}")
-                                print(f"批次: {result['batch_name']}\n")
-
-                                # 获取appositive_fraction数组中的参数
-                                for app_fraction in result["appositive_fraction"]:
-                                    ls_year1 = app_fraction["year"]
-                                    ls_score1 = app_fraction["score"]
-                                    ls_rank_range1 = app_fraction["rank_range"]
-                                    print(f"{ls_year1}年同位次考生得分:{ls_score1}, 排名区间：{ls_rank_range1}")
-                            print()  # 每个结果之间用空行分隔
+                        continue_search = input(Fore.GREEN + "是否需要继续查询？（按 Y 继续，按 N 返回上级菜单，默认：y，按 Enter 键继续查询）：" + Style.RESET_ALL) or 'y'
+                        print()
+                        if continue_search.lower() == 'y':
+                            break  # 跳出内层循环，继续查询
+                        elif continue_search.lower() == 'n':
+                            break  # 跳出内层循环，返回上级菜单
                         else:
-                            print(Fore.RED + "未找到与输入内容匹配的数据。" + Style.RESET_ALL)
+                            print(Fore.RED + "错误：请输入 Y 或 N 。" + Style.RESET_ALL)
 
-                        # 提示询问用户是否继续查询
-                        while True:
-                            continue_search = input(Fore.GREEN + "是否需要继续查询？（按 Y 继续，按 N 返回上级菜单，默认：y，按 Enter 键继续查询）：" + Style.RESET_ALL) or 'y'
-                            print()
-                            if continue_search.lower() == 'y':
-                                break  # 跳出内层循环，继续查询
-                            elif continue_search.lower() == 'n':
-                                break  # 跳出内层循环，返回上级菜单
-                            else:
-                                print(Fore.RED + "错误：请输入 Y 或 N 。" + Style.RESET_ALL)
-
-                        if continue_search.lower() == 'n':
-                            os.system('cls' if os.name == 'nt' else 'clear')
-                            break  # 返回上级菜单
-                    break  # 跳出当前层循环，返回到主菜单
-                break  # 返回到主菜单
-
+                    if continue_search.lower() == 'n':
+                        os.system('cls' if os.name == 'nt' else 'clear')
+                        break  # 返回上级菜单
+                break  # 跳出当前层循环，返回到主菜单
             elif choice == '2':
                 while True:
                     gk_year = input(Fore.GREEN + "\n请输入年份" + Fore.RED + "（2016 - 2028之间的年份，默认值为2024）: " + Style.RESET_ALL) or "2024"
